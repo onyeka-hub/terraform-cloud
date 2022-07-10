5064413001ff1d73f77340332c16b769224900fd
onyeka-hub <onyekagodonu@yahoo.com>
2022-07-10 17:38:43 +0100

    update

---
+++ outputs.tf
@@ -0,0 +1,33 @@
+output "ext_lb_dns_name" {
+  value = module.alb.ext_lb_dns_name
+}
+
+output "Jenkins-public-ip" {
+  value       = module.compute.Jenkins-public-ip
+  description = "Jenkins public ip"
+}
+
+output "Jenkins-instance-state" {
+  value       = module.compute.Jenkins-instance-state
+  description = "Jenkins instance state"
+}
+
+output "sonarqube-public-ip" {
+  value       = module.compute.sonarqube-public-ip
+  description = "Jenkins public ip"
+}
+
+output "sonarqube-instance-state" {
+  value       = module.compute.sonarqube-instance-state
+  description = "Jenkins instance state"
+}
+
+output "artifactory-public-ip" {
+  value       = module.compute.artifactory-public-ip
+  description = "Jenkins public ip"
+}
+
+output "artifactory-instance-state" {
+  value       = module.compute.artifactory-instance-state
+  description = "Jenkins instance state"
+}
