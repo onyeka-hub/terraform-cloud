7aee7c9ba80037e14e3c58bd94cc4e8af8953503
onyeka-hub <onyekagodonu@yahoo.com>
2022-07-11 14:43:03 +0100

    actual infrastructure

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -1,19 +1,19 @@
 # create instance for jenkins
-# resource "aws_instance" "Jenkins" {
-#   ami                         = var.ami-jenkins
-#   instance_type               = "t2.micro"
-#   subnet_id                   = var.subnets-compute
-#   vpc_security_group_ids      = var.sg-compute
-#   associate_public_ip_address = true
-#   key_name                    = var.keypair
+resource "aws_instance" "Jenkins" {
+  ami                         = var.ami-jenkins
+  instance_type               = "t2.micro"
+  subnet_id                   = var.subnets-compute
+  vpc_security_group_ids      = var.sg-compute
+  associate_public_ip_address = true
+  key_name                    = var.keypair
 
-#  tags = merge(
-#     var.tags,
-#     {
-#       Name = "onyi-Jenkins"
-#     },
-#   )
-# }
+ tags = merge(
+    var.tags,
+    {
+      Name = "onyi-Jenkins"
+    },
+  )
+}
 
 
 #create instance for sonarqube
