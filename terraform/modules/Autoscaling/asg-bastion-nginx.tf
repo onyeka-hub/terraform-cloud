e58c642bd0b9686b5100a96c853e0aa0f385ce42
onyeka-hub <onyekagodonu@yahoo.com>
2022-07-11 19:51:20 +0100

    commenting out listeners

--- terraform/modules/Autoscaling/asg-bastion-nginx.tf
+++ terraform/modules/Autoscaling/asg-bastion-nginx.tf
@@ -75,7 +75,7 @@ vpc_zone_identifier = var.public_subnets
 
 # attaching autoscaling group of nginx to external load balancer
 
-resource "aws_autoscaling_attachment" "asg_attachment_nginx" {
-  autoscaling_group_name = aws_autoscaling_group.nginx-asg.id
-  alb_target_group_arn   = var.nginx-alb-tgt
-}
+# resource "aws_autoscaling_attachment" "asg_attachment_nginx" {
+#   autoscaling_group_name = aws_autoscaling_group.nginx-asg.id
+#   alb_target_group_arn   = var.nginx-alb-tgt
+# }
