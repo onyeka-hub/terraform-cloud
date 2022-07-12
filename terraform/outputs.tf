0560ae296762569ab8c253e8abad426ac2bc63bf
onyeka-hub <onyekagodonu@yahoo.com>
2022-07-12 13:54:34 +0100

    dont fail

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,32 +2,32 @@ output "ext_lb_dns_name" {
   value = module.alb.ext_lb_dns_name
 }
 
-output "Jenkins-public-ip" {
-  value       = module.compute.Jenkins-public-ip
-  description = "Jenkins public ip"
-}
+# output "Jenkins-public-ip" {
+#   value       = module.compute.Jenkins-public-ip
+#   description = "Jenkins public ip"
+# }
 
-output "Jenkins-instance-state" {
-  value       = module.compute.Jenkins-instance-state
-  description = "Jenkins instance state"
-}
+# output "Jenkins-instance-state" {
+#   value       = module.compute.Jenkins-instance-state
+#   description = "Jenkins instance state"
+# }
 
-output "sonarqube-public-ip" {
-  value       = module.compute.sonarqube-public-ip
-  description = "Jenkins public ip"
-}
+# output "sonarqube-public-ip" {
+#   value       = module.compute.sonarqube-public-ip
+#   description = "Jenkins public ip"
+# }
 
-output "sonarqube-instance-state" {
-  value       = module.compute.sonarqube-instance-state
-  description = "Jenkins instance state"
-}
+# output "sonarqube-instance-state" {
+#   value       = module.compute.sonarqube-instance-state
+#   description = "Jenkins instance state"
+# }
 
-output "artifactory-public-ip" {
-  value       = module.compute.artifactory-public-ip
-  description = "Jenkins public ip"
-}
+# output "artifactory-public-ip" {
+#   value       = module.compute.artifactory-public-ip
+#   description = "Jenkins public ip"
+# }
 
-output "artifactory-instance-state" {
-  value       = module.compute.artifactory-instance-state
-  description = "Jenkins instance state"
-}
+# output "artifactory-instance-state" {
+#   value       = module.compute.artifactory-instance-state
+#   description = "Jenkins instance state"
+# }
